e4f5ccdbe671fd13dfb0282719f90fc4b93b1962
kevinmaulanan <60502244+kevinmaulanan@users.noreply.github.com>
2021-05-14 23:20:53 +0700

    Tugas 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
-import Lifecycle from './Lifecycle';
+import "./App.css";
+import KTP from "./KTP";
+import StateManagement from "./StateManagement";
+import ID from "./ID";
+import BlogSpot from "./BlogSpot";
+import Posts from "./Posts";
+
 function App() {
   return (
     <div className="App">
-      <Lifecycle />
+      {/* <KTP
+        namaDepan="Budi"
+        namaBelakang="H"
+        tahunKelahiran={1990}
+      />
+      <KTP
+        namaDepan="Handoko"
+        namaBelakang="Halim"
+        tahunKelahiran={1970}
+        alamat="Semarang"
+      />
+      <StateManagement
+        initialValue=""
+      />
+
+      <ID /> */}
+      {/* <BlogSpot /> */}
+      <Posts />
     </div>
   );
 }
